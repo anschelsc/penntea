3aa700b39f3dca0f1ef0b67755c9fc136928b745
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2017-04-28 19:28:05 -0400

    Slightly more of a real page.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
-package hello
+package tea
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 	"time"
 
@@ -9,7 +9,32 @@ import (
 	"appengine/datastore"
 )
 
+type pageInfo struct {
+	T      string
+	Recent bool
+}
+
+const page = `
+<html>
+<head>
+<title>Is there tea yet?</title>
+</head>
+
+<body>
+{{if .Recent}}
+Probably!
+{{else}}
+Probably not.
+{{end}}
+Tea was last reported on {{.T}}. To report tea yourself, click <a href="/set">here</a>.
+</body>
+`
+
+var pageT = template.Must(template.New("page").Parse(page))
+
 var philly *time.Location
+
+const longEnough = 2 * time.Hour
 
 func init() {
 	var err error
@@ -20,14 +45,19 @@ func init() {
 	http.HandleFunc("/set", setTime)
 }
 
-type Last struct {
+func recent(t time.Time) bool {
+	d := time.Since(t)
+	return d < longEnough
+}
+
+type last struct {
 	T time.Time
 }
 
 func setTime(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	k := datastore.NewKey(c, "time", "last", 0, nil)
-	if _, err := datastore.Put(c, k, &Last{time.Now()}); err != nil {
+	if _, err := datastore.Put(c, k, &last{time.Now()}); err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
@@ -37,10 +67,13 @@ func setTime(w http.ResponseWriter, r *http.Request) {
 func getTime(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	k := datastore.NewKey(c, "time", "last", 0, nil)
-	l := new(Last)
+	l := new(last)
 	if err := datastore.Get(c, k, l); err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-	fmt.Fprint(w, l.T.In(philly).Format("January 2 (Monday) at 3:04 PM"))
+	pageT.Execute(w, &pageInfo{
+		T:      l.T.In(philly).Format("January 2 (Monday) at 3:04 PM"),
+		Recent: recent(l.T),
+	})
 }
